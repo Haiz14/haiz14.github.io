0f86e4e2f43f10d5ab3b9cb8dec0aae89be8a703
Haiz14 <haiz.gigs@gmail.com>
2023-11-17 10:45:22 +0530

    feat: created seoerate apo bar component

---
+++ src/lib/AppBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { DrawerSettings } from '@skeletonlabs/skeleton';
+	import { AppBar, LightSwitch, popup, getModalStore } from '@skeletonlabs/skeleton';
+	import {getDrawerStore} from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+	function drawerOpen(): void {
+		const s: DrawerSettings = { id: 'doc-sidenav' };
+		drawerStore.open(s);
+	}
+</script>
+
+<AppBar shadow="shadow-2xl" slotTrail="!space-x-2">
+			<button on:click={drawerOpen} class="btn-icon btn-icon-sm lg:!hidden">
+				<i class="fa-solid fa-bars text-xl" />
+			</button>
+</AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+	/**
+	add side bar compinent
+	add app bar component
+	add app shell
+	*/
+	// css
 	import '../app.postcss';
 	import '@fortawesome/fontawesome-free/css/fontawesome.css';
 	import '@fortawesome/fontawesome-free/css/brands.css';
 	import '@fortawesome/fontawesome-free/css/solid.css';
 
+	import AppBar from '$lib/AppBar.svelte'
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup, initializeStores } from '@skeletonlabs/skeleton';
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
 	initializeStores();
 </script>
-
+<AppBar/>
 <slot />
