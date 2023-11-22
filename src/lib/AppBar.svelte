71648d41912961325d28b931a4e444597af420c6
Haiz14 <haiz.gigs@gmail.com>
2023-11-22 17:07:54 +0530

    feat: added logo, hamburger menu and lightswitch to app har

--- src/lib/AppBar.svelte
+++ src/lib/AppBar.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	/**
+	- 💤 make header disapper on scroll down
+	- 💤 make bar appear on scroll up
+	*/
 	import type { DrawerSettings } from '@skeletonlabs/skeleton';
-	import { AppBar, LightSwitch, popup, getModalStore } from '@skeletonlabs/skeleton';
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 	import {getDrawerStore} from '@skeletonlabs/skeleton';
 
 	const drawerStore = getDrawerStore();
@@ -11,7 +15,18 @@
 </script>
 
 <AppBar shadow="shadow-2xl" slotTrail="!space-x-2">
+	<svelte:fragment slot="lead">
+			<!-- Logo -->
+			<a class="h1" href="/" title="home">
+				haiz.dev
+			</a>
+	</svelte:fragment>
+
+	<svelte:fragment slot="trail">
+			<LightSwitch/>
+			<!-- Hburger menu -->
 			<button on:click={drawerOpen} class="btn-icon btn-icon-sm lg:!hidden">
 				<i class="fa-solid fa-bars text-xl" />
 			</button>
+	</svelte:fragment>
 </AppBar>
