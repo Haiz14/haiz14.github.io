056f3097e91f50a4df14829185eed1f4c4f0ac0b
haiz <haiz.gigs@gmail.com>
2024-11-18 09:24:46 +0000

    fixd: top padding on igger screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import content from "$lib/content.json";
 </script>
 
-<div class="my-20 mx-4 space-y-10">
+<div class="my-20 mx-6 md:mx-[10%] lg:mx-[20%] space-y-10">
 	<div class="space-y-6">
 		<h1 class="text-5xl"> {content.quote} </h1>
 		<h4 class="text-2xl leading-10"> {content.quote_expansion} </h4>
