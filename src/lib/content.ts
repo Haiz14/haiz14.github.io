1eb42decdc324994abf0ad69b6f9a7e339b5ec45
Haiz14 <haiz.gigs@gmail.com>
2025-02-06 11:27:46 +0530

    feat: json to ts file

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,8 +1,10 @@
-{
-  "quote": "To live is to act",
+const content = {
+  "quote": "Life's a but boring. Design brings r",
 	"quote_expansion": "will you send me my script? a script for a programmer? a script for an app that speeds up your workflow? An app that brings ease to your business.",
   "email_address": "haiz.gigs@gmail.com",
   "cta": "email the script",
   "subject": "Query for building internal tools to speed up our workflow",
   "pricing": "hourly USD 25"
-}
+};
+
+export default content;
