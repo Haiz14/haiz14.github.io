800797392bca8f42cb1905ae4bfe742f1ad1acba
Haiz14 <haiz.gigs@gmail.com>
2025-02-06 12:10:52 +0530

    feat: main part content ✅

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,10 +1,12 @@
 const content = {
-  "quote": "Life's a but boring. Design brings r",
-	"quote_expansion": "will you send me my script? a script for a programmer? a script for an app that speeds up your workflow? An app that brings ease to your business.",
-  "email_address": "haiz.gigs@gmail.com",
-  "cta": "email the script",
-  "subject": "Query for building internal tools to speed up our workflow",
-  "pricing": "hourly USD 25"
+  "hero_glimpse": {
+    "main_line": "'Great desgins are easy to understnad while leaving a lasting impression'. This quotation is easy to understand, does it leave a lasting imoression; probably nooo!!!",
+    "secondary_line": "'Leaving a lasting impression' about  you, your business or your products and services; is what i wanna learn, is what i am learning and i'll become a master at it!! ✨",
+    "tertiary_line": "let me design a website for you?",
+    "action_text": "email me (i'll be happy 😸)",
+    "beautiful_image_url": "https://asset.cloudinary.com/dnhp82n1n/08a9909f4bbb4676b6df4329d617edfc",
+    "beautiful_image_alt": "Beautiful dark purple flower.",
+  },
 };
 
 export default content;
